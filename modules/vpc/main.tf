746a09a2f880354d980c5f13d58a3e77844bde0e
sathyamoorthy078 <sathyamoorthy078@gmail.com>
2026-08-24 00:04:36 +0530

    update1

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -11,7 +11,7 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "${var.vpc_name}-igw"
+    Name = "${var.vpc_name}-igw" 
   }
 }
 resource "aws_subnet" "public" {
